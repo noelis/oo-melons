d6455eab0e40891fe261ec47dc04cd40e220c69a
Hackbright Student <no-reply@hackbrightacademy.com>
2016-10-07 12:26:36 -0700

    refactored code

--- melons.py
+++ melons.py
@@ -4,12 +4,12 @@ from random import randint
 class AbstractMelonOrder(object):
     """Covers all orders, domestic and international"""
     
-    def __init__(self, species, qty, country_code, order_type, tax):
+    def __init__(self, species, qty, country_code=None, order_type=None, tax=None):
         self.species = species
         self.qty = qty
-        self.country_code = country_code
-        self.order_type = order_type
-        self.tax = tax
+        # self.country_code = country_code
+        # self.order_type = order_type
+        # self.tax = tax
         self.shipped = False
 
     def mark_shipped(self):
@@ -32,18 +32,23 @@ class AbstractMelonOrder(object):
         return total
 
 class DomesticMelonOrder(AbstractMelonOrder):
-    def __init__(self, species, qty):
-        super(DomesticMelonOrder, self).__init__(species, qty, "USA", "domestic", 0.08)
+    country_code = "USA"
+    order_type = "domestic"
+    tax = 0.08
+    # def __init__(self, species, qty):
+    #     super(DomesticMelonOrder, self).__init__(species, qty, "USA", "domestic", 0.08)
 
 class InternationalMelonOrder(AbstractMelonOrder):
-    def __init__(self, species, qty, country_code):
-        super(InternationalMelonOrder, self).__init__(species, qty, country_code, 
-            "international", 0.17)
+    order_type = "international"
+    tax = 0.17
+    # def __init__(self, species, qty, country_code):
+    #     super(InternationalMelonOrder, self).__init__(species, qty, country_code, 
+    #         "international", 0.17)
 
-class GovernmentMelonOrder(AbstractMelonOrder):
-    def __init__(self, species, qty):
-        super(GovernmentMelonOrder, self).__init__(species, qty, "USA", "domestic", 0.08)
-        self.passed_inspection = False
-
+class GovernmentMelonOrder(DomesticMelonOrder):
+    passed_inspection = False
+    # def __init__(self, species, qty):
+    #     super(GovernmentMelonOrder, self).__init__(species, qty, "USA", "domestic", 0.08)
+    
     def mark_inspection(self, passed):
         self.passed_inspection = passed
